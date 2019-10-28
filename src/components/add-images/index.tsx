5e94092b8ff407c9252064f56faac70f406a75ad
noUser <noUser>
2019-10-28 18:23:16 +0200

    fix from class

--- src/components/add-images/index.tsx
+++ src/components/add-images/index.tsx
@@ -13,123 +13,125 @@ import { getCountries, saveImageAction } from "../../redux/actions";
 import css from "./style.module.css";
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap"
-  },
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2)
-  }
+    root: {
+        display: "flex",
+        flexWrap: "wrap"
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2)
+    }
 }));
 
 class AddImages extends React.Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = { current: "ISR", image: "" };
-  }
+    constructor(props: any) {
+        super(props);
+        this.state = { current: "ISR", image: "" };
+    }
 
-  componentDidMount() {
-    const { getCountries } = this.props.reduxActions;
-    getCountries();
-  }
+    componentDidMount() {
+        const { getCountries } = this.props.reduxActions;
+        getCountries();
+    }
 
-  render() {
-    const { countries, countriesLoading } = this.props;
+    render() {
+        const { countries, countriesLoading } = this.props;
+        console.log(countriesLoading, "countriesLoading")
+        // if (countriesLoading) return <div className={css.loader}></div>;
+        // // if (!countries.length) return <h1> no data...</h1>;
 
-    if (countriesLoading) return <div className={css.loader}></div>;
-    if (!countries.length) return <h1> no data...</h1>;
+        return (
+            <div>
+                <div>
+                    <FormControl>
 
-    return (
-      <div>
-        <div>
-          <FormControl>
-            <InputLabel>Countries</InputLabel>
-            <Select
-              value={this.state.current}
-              onChange={(e: any) => this.setState({ current: e.target.value })}
-            >
-              {genreateMenuItems(countries)}
-            </Select>
-            <input
-              onChange={(e: any) => {
-                this.setState({ image: e.target.value });
-              }}
-              type="text"
-            />
-            <button
-              onClick={() => {
-                const { saveImage } = this.props.reduxActions;
-                saveImage(this.state);
-              }}
-              className="btn btn-success"
-            >
-              {" "}
-              Save image{" "}
-            </button>
-          </FormControl>
-        </div>
-      </div>
-    );
-  }
+                        {countriesLoading ? <div className={css.loader}></div> : <>
+                            <InputLabel>Countries</InputLabel>
+                            <Select
+                                value={this.state.current}
+                                onChange={(e: any) => this.setState({ current: e.target.value })}
+                            >
+                                {genreateMenuItems(countries)}
+                            </Select>
+                        </>}
+                        <input
+                            onChange={(e: any) => {
+                                this.setState({ image: e.target.value });
+                            }}
+                            type="text"
+                        />
+                        <button
+                            onClick={() => {
+                                const { saveImage } = this.props.reduxActions;
+                                saveImage(this.state);
+                            }}
+                            className="btn btn-success"
+                        >
+
+                            Save image{" "}
+                        </button>
+                    </FormControl>
+                </div>
+            </div>
+        );
+    }
 }
 
 function genreateMenuItems(countries: Array<Country>) {
-  console.log(countries);
-  return countries.map((country: Country) => {
-    console.log(country.alpha3Code);
-    return (
-      <MenuItem key={country.code} value={country.code}>
-        {country.name}
-      </MenuItem>
-    );
-  });
+    return countries.map((country: Country) => {
+        const { code, name } = country;
+        return (
+            <MenuItem key={code} value={code}>
+                {name}
+            </MenuItem>
+        );
+    });
 }
 
 interface Country {
-  capital: string;
-  name: string;
-  alpha3Code: string;
-  code: string;
-  region: string;
-  borders: Array<string>;
-  flag: string;
+    capital: string;
+    name: string;
+    alpha3Code: string;
+    code: string;
+    region: string;
+    borders: Array<string>;
+    flag: string;
 }
 const mapStateToProps = (state: any) => {
-  const { countries, countriesLoading } = state;
-  const mappedCountries = countries.map((country: Country) => {
-    const { capital, name, alpha3Code, region, borders, flag } = country;
-    return {
-      capital: capital,
-      name: name,
-      code: alpha3Code,
-      region: region,
-      borders: borders,
-      flag: flag
-    };
-  });
-  return { countries: mappedCountries, countriesLoading };
+    const { countries, countriesLoading } = state;
+    const mappedCountries = countries.map((country: Country) => {
+        const { capital, name, alpha3Code, region, borders, flag } = country;
+        return {
+            capital: capital,
+            name: name,
+            code: alpha3Code,
+            region: region,
+            borders: borders,
+            flag: flag
+        };
+    });
+    return { countries: mappedCountries, countriesLoading };
 };
 
 const mapDispatchToProps = (dispatch: any) => {
-  return {
-    //this.props.reduxActions.getCountries?
-    reduxActions: {
-      getCountries: () => {
-        dispatch(getCountries());
-      },
-      saveImage: (payload: any) => {
-        dispatch(
-          saveImageAction({ code: payload.current, image: payload.image })
-        );
-      }
-    }
-  };
+    return {
+        //this.props.reduxActions.getCountries?
+        reduxActions: {
+            getCountries: () => {
+                dispatch(getCountries());
+            },
+            saveImage: (payload: any) => {
+                dispatch(
+                    saveImageAction({ code: payload.current, image: payload.image })
+                );
+            }
+        }
+    };
 };
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(AddImages);
--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -2,56 +2,55 @@ import Actions from "./actions.config";
 
 const imagesCountries: { [key: string]: any[] } = {};
 const initialState = {
-  user: null,
-  countries: [],
-  countriesLoading: false,
-  imagesCountries
+    user: null,
+    countries: [],
+    countriesLoading: false,
+    imagesCountries
 };
 
 export default function root(state = initialState, action: any) {
-  console.log(action.payload);
-  switch (action.type) {
-    case Actions.SAVE_USER: {
-      // return equals to global set state - setting the store
-      return {
-        ...state,
-        user: "hello " + action.payload.user + "@gmail.com"
-      };
-    }
-
-    // deprecated in version 24.10.209
-    case Actions.GET_COUNTRIES: {
-      // some logic
-      return {
-        ...state,
-        countries: ["ISR", "AFG"]
-      };
-    }
-
-    case Actions.SAVE_IMAGES_ACTION: {
-      const { code, image } = action.payload;
-      const { imagesCountries } = state;
-      const isCodeExist = imagesCountries.hasOwnProperty(code);
-      return {
-        ...state,
-        imagesCountries: {
-          ...imagesCountries,
-          [code]: isCodeExist ? [...imagesCountries[code], image] : [image]
+    switch (action.type) {
+        case Actions.SAVE_USER: {
+            // return equals to global set state - setting the store
+            return {
+                ...state,
+                user: "hello " + action.payload.user + "@gmail.com"
+            };
         }
-      };
-    }
 
-    case Actions.GET_COUNTRIES_SUCCESS: {
-      const { countries } = action.payload;
-      return { ...state, countries, countriesLoading: false };
-    }
+        // deprecated in version 24.10.209
+        case Actions.GET_COUNTRIES: {
+            // some logic
+            return {
+                ...state,
+                countries: ["ISR", "AFG"]
+            };
+        }
 
-    case Actions.GET_COUNTRIES_PENDING: {
-      return { ...state, countriesLoading: true };
-    }
+        case Actions.SAVE_IMAGES_ACTION: {
+            const { code, image } = action.payload;
+            const { imagesCountries } = state;
+            const isCodeExist = imagesCountries.hasOwnProperty(code);
+            return {
+                ...state,
+                imagesCountries: {
+                    ...imagesCountries,
+                    [code]: isCodeExist ? [...imagesCountries[code], image] : [image]
+                }
+            };
+        }
 
-    default: {
-      return state;
+        case Actions.GET_COUNTRIES_SUCCESS: {
+            const { countries } = action.payload;
+            return { ...state, countries, countriesLoading: false };
+        }
+
+        case Actions.GET_COUNTRIES_PENDING: {
+            return { ...state, countriesLoading: true };
+        }
+
+        default: {
+            return state;
+        }
     }
-  }
 }
